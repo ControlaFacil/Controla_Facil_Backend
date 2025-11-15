cc942389982a97d4092c090cd26d0795a6e2ab64
Guilherme Galdino <guigaldino.sp@gmail.com>
2025-11-15 14:00:25 -0300

    Update integracaoController.js

--- src/controllers/integracaoController.js
+++ src/controllers/integracaoController.js
@@ -42,10 +42,10 @@ const integracaoController = {
          "https://api.mercadolibre.com/oauth/token",
          {
            grant_type: "authorization_code",
-           client_id: process.env.ML_CLIENT_ID,
-           client_secret: process.env.ML_CLIENT_SECRET,
+           client_id: "5137782277166166",
+           client_secret: "g1naoZfuSzwlMuz40ZRmFia6rPfIZF7Q",
            code,
-           redirect_uri: process.env.ML_REDIRECT_URI,
+           redirect_uri: "https://controlafacilbackend-production.up.railway.app/api/integracoes/callback",
          },
          {
            headers: { "Content-Type": "application/x-www-form-urlencoded" },
